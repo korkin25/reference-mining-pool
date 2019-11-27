54e828f63309ee1950735e585a345da3096579b6
xmvdev <xmvdev@gmail.com>
2019-11-27 13:02:39 +0000

    genesis

---
+++ html/config.js
@@ -0,0 +1,20 @@
+var api = "/rpc";
+
+var poolHost = "pool.monerov.online";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+
+var guiWallet = "https://github.com/xmvdev/monerov-gui/releases/latest";
+
+var paperWallet = "paperwallet.html";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
+
+var blockchainExplorer = "https://explorer.monerov.online/block/{id}";
+
+var transactionExplorer = "https://explorer.monerov.online/tx/{id}";
